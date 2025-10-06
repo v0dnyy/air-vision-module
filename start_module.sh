0307563acb0137e70ed5bb4276f4e02d6f1a8578
v0dnyy <sv18082002@mail.ru>
2025-10-06 23:39:19 +0400

    new script for startup

---
+++ start_module.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+SCRIPT_TO_RUN="inference.py"
+SCRIPT_ARGS=(
+    --path_to_model_w "yolo_n_v11_dropout_best.pt"
+    --from_cam
+    --show_video
+    --save_video
+    --save_logs
+)
+
+echo "Запускаем скрипт $SCRIPT_TO_RUN..."
+
+python "$SCRIPT_TO_RUN" "${SCRIPT_ARGS[@]}"
+
+echo "=== Скрипт завершил работу ==="
